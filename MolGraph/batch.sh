035607ee27365b4966f523d4e200f164541322a7
Christopher <78366262+CondaPereira@users.noreply.github.com>
2022-04-27 14:41:43 +0800

    Add files via upload

---
+++ MolGraph/batch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+for ((i=1; i<=996; i++));
+do
+	mv 'geomparm out '$i.txt test_$i.txt
+	mkdir test_$i
+	mv test_$i.txt /home/szk/sdf/txt/test_$i/test_$i.txt
+	cd test_$i
+	awk '{print $1,$2,$3}' /home/szk/sdf/txt/test_$i/test_$i.txt > real_$i.txt
+	cd ..
+	cd test_$i
+	awk '{print $1}' real_$i.txt > list1.txt
+	awk '{print $2}' real_$i.txt > list2.txt
+	awk '{print $3}' real_$i.txt > list3.txt
+	cd ..
+	/home/szk/miniconda3/bin/python normalizer.py -i /home/szk/sdf/txt/test_$i/list1.txt -o /home/szk/sdf/txt/test_$i/1.txt
+	/home/szk/miniconda3/bin/python normalizer.py -i /home/szk/sdf/txt/test_$i/list2.txt -o /home/szk/sdf/txt/test_$i/2.txt
+	/home/szk/miniconda3/bin/python normalizer.py -i /home/szk/sdf/txt/test_$i/list3.txt -o /home/szk/sdf/txt/test_$i/3.txt
+	cd test_$i
+	while read number; do echo -n "0x"; echo "obase=16; ibase=10; $number" | bc; done < 1.txt > a1.txt
+	while read number; do echo -n "0x"; echo "obase=16; ibase=10; $number" | bc; done < 2.txt > a2.txt
+	while read number; do echo -n "0x"; echo "obase=16; ibase=10; $number" | bc; done < 3.txt > a3.txt
+	paste a1.txt a2.txt a3.txt >> all.txt
+	paste 1.txt 2.txt 3.txt >> dec.txt
+	cd ..
+done
+
+for ((j=1; j<=996; j++));
+do
+	cd test_$j
+	n=`grep -c "" dec.txt`
+	for ((k=1; k<=n; k++));
+	do
+		head -$k dec.txt | tail -n +$k > rgb_temp.txt
+		echo "testbmp go.bmp 3 100 100 " > command_front.txt
+		paste command_front.txt rgb_temp.txt > command.sh && chmod +x command.sh && ./command.sh && mv go.bmp go_$k.bmp
+		rm -rf rgb_temp.txt
+		rm -rf command.sh
+	done
+	rm -rf *.txt
+	cd ..
+	/home/szk/miniconda3/bin/python /home/szk/sdf/txt/merge.py -i /home/szk/sdf/txt/test_$j
+	rm -rf test_$j
+done
+
---
+++ MolGraph/merge.py
@@ -0,0 +1,99 @@
+import os
+
+import PIL.Image as Image
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required = True,
+                help = "path to our in processed files")
+args = vars(ap.parse_args())
+
+def resize_by_width(infile, image_size):
+    """按照宽度进行所需比例缩放"""
+    im = Image.open(infile)
+    (x, y) = im.size
+    lv = round(x / image_size, 2) + 0.01
+    x_s = int(x // lv)
+    y_s = int(y // lv)
+    print("x_s", x_s, y_s)
+    out = im.resize((x_s, y_s), Image.ANTIALIAS)
+    return out
+
+
+def get_new_img_xy(infile, image_size):
+    """返回一个图片的宽、高像素"""
+    im = Image.open(infile)
+    (x, y) = im.size
+    lv = round(x / image_size, 2) + 0.01
+    x_s = x // lv
+    y_s = y // lv
+    # print("x_s", x_s, y_s)
+    # out = im.resize((x_s, y_s), Image.ANTIALIAS)
+    return x_s, y_s
+
+
+# 定义图像拼接函数
+def image_compose(image_colnum, image_size, image_rownum, image_names, image_save_path, x_new, y_new):
+    to_image = Image.new('RGB', (image_colnum * x_new, image_rownum * y_new))  # 创建一个新图
+    # 循环遍历，把每张图片按顺序粘贴到对应位置上
+    total_num = 0
+    for y in range(1, image_rownum + 1):
+        for x in range(1, image_colnum + 1):
+            from_image = resize_by_width(image_names[image_colnum * (y - 1) + x - 1], image_size)
+            # from_image = Image.open(image_names[image_colnum * (y - 1) + x - 1]).resize((image_size,image_size ), Image.ANTIALIAS)
+            to_image.paste(from_image, ((x - 1) * x_new, (y - 1) * y_new))
+            total_num += 1
+            if total_num == len(image_names):
+                break
+    return to_image.save(image_save_path)  # 保存新图
+
+
+def get_image_list_fullpath(dir_path):
+    file_name_list = os.listdir(dir_path)
+    image_fullpath_list = []
+    for file_name_one in file_name_list:
+        file_one_path = os.path.join(dir_path, file_name_one)
+        if os.path.isfile(file_one_path):
+            image_fullpath_list.append(file_one_path)
+        else:
+            img_path_list = get_image_list_fullpath(file_one_path)
+            image_fullpath_list.extend(img_path_list)
+    return image_fullpath_list
+
+
+def merge_images(image_dir_path,image_size,image_colnum):
+    # 获取图片集地址下的所有图片名称
+    image_fullpath_list = get_image_list_fullpath(image_dir_path)
+    print("image_fullpath_list", len(image_fullpath_list), image_fullpath_list)
+
+    image_save_path = r'{}.bmp'.format(image_dir_path)  
+    # image_rownum = 4  
+    image_rownum_yu = len(image_fullpath_list) % image_colnum
+    if image_rownum_yu == 0:
+        image_rownum = len(image_fullpath_list) // image_colnum
+    else:
+        image_rownum = len(image_fullpath_list) // image_colnum + 1
+
+    x_list = []
+    y_list = []
+    for img_file in image_fullpath_list:
+        img_x, img_y = get_new_img_xy(img_file, image_size)
+        x_list.append(img_x)
+        y_list.append(img_y)
+
+    print("x_list", sorted(x_list))
+    print("y_list", sorted(y_list))
+    x_new = int(x_list[len(x_list) // 5 * 4])
+    y_new = int(y_list[len(y_list) // 5 * 4])
+    print(" x_new, y_new", x_new, y_new)
+    image_compose(image_colnum, image_size, image_rownum, image_fullpath_list, image_save_path, x_new, y_new)  # 调用函数
+    # for img_file in image_fullpath_list:
+    #     resize_by_width(img_file,image_size)
+
+if __name__ == '__main__':
+
+    image_dir_path = args['input'] 
+    image_size = 64  
+    image_colnum = 9  # 合并成一张图后，一行有几个小图
+    merge_images(image_dir_path, image_size, image_colnum)
+    
