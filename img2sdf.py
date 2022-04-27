b2cedb0ffe753d63e1463f70f4252cf12e1c8579
Christopher <78366262+CondaPereira@users.noreply.github.com>
2022-04-27 21:16:19 +0800

    Update and rename pngtosdf.py to img2sdf.py

---
+++ img2sdf.py
@@ -0,0 +1,53 @@
+import os
+import torch
+from img2mol.inference import *
+
+os.listdir("model/")
+# point to our own training dataset
+device = "cuda:0"
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+img2mol = Img2MolInference(model_ckpt = "model/model.ckpt", device=device)
+
+cddd_server = CDDDRequest()
+
+class BatchRename():
+    def __init__(self):
+        self.path = 'RAD51_molp/'  
+
+    def rename(self):
+        filelist = os.listdir(self.path)   
+        total_num = len(filelist)  
+        i = 1  
+        for item in filelist:
+            if item.endswith('.jpg') or item.endswith('.png'):  
+                src = os.path.join(os.path.abspath(self.path), item)
+                dst = os.path.join(os.path.abspath(self.path), '0' + format(str(i), '0>2s') + '.jpg')#处理后的格式也为jpg格式的，当然这里可以改成png格式
+                # restart to define our picture's name label
+                try:
+                    os.rename(src, dst)
+                    print ('converting %s to %s ...' % (src, dst))
+                    i = i + 1
+                except:
+                    continue
+        print ('total %d to rename & converted %d jpgs' % (total_num, i))
+
+if __name__ == '__main__':
+    demo = BatchRename()
+    demo.rename()
+
+os.system('cat > smiles_out.txt && chmod +x smiles_out.txt')
+f = open("smiles_out.txt",'w+')
+f.close
+def read_path(file_pathname):
+    
+    for filename in os.listdir(file_pathname):
+        res = img2mol(file_pathname+'/'+filename, cddd_server=cddd_server)
+        res["mol"]
+        print(res["smiles"],file=f)
+
+read_path("RAD51_molp/")
